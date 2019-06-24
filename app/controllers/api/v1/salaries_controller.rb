47cc1b1e27f6c02d5c7db081270bdb7219431258
Nora Gabriela Alvarado <noragmora@gmail.com>
2019-06-24 14:48:30 -0500

    Added salaries controller

---
+++ app/controllers/api/v1/salaries_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SalariesController < ApplicationController
+      def index
+        render json: SalaryCalculatorService.call(team_params[:_json]), status: 200
+      end
+
+      private
+
+      def team_params
+        params.permit(_json: [:nombre,
+                              :nivel,
+                              :goles,
+                              :sueldo,
+                              :bono,
+                              :sueldo_completo,
+                              :equipo])
+      end
+    end
+  end
+end
